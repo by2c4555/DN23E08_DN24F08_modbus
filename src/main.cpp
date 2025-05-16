942674cd532411566dde3ba6723aa9bc0be9cb99
by2c4555 <apisitputtasri@gmail.com>
2025-05-16 15:07:47 +0700

    update EEPROM

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,7 @@
 
 #include <ModbusRTUSlave.h>
 #define MODBUS_SERIAL Serial
-#define MODBUS_BAUD 9600
 #define MODBUS_CONFIG SERIAL_8N1
-#define MODBUS_UNIT_ID 1
 #define dePin 13
 //Pin connected to latch of Digital Tube Module
 int latchPin = 11;
@@ -29,7 +27,11 @@ const int K3 =7;
 const int K4 = 8;
 
 DN23E08_IO gpio(latchPin,clockPin,dataPin,OE_595,LOAD_165,DATA_165,CLK_165);
+ModbusRTUSlave modbus(MODBUS_SERIAL, dePin);
+uint16_t USART_BAUD[] = {300,600,1200,2400,4800,9600,14400,19200};
+uint16_t MODBUS_UNIT_ID = 1;
 int count = 0;
+
 void DN23E08Init(){
   gpio.begin();
   gpio.RelayClear(2);
@@ -38,10 +40,9 @@ void DN23E08Init(){
   pinMode(K3, INPUT);
   pinMode(K4, INPUT);
 }
-ModbusRTUSlave modbus(MODBUS_SERIAL, dePin);
 const uint8_t numCoils = 8;
 const uint8_t numDiscreteInputs = 8;
-const uint8_t numHoldingRegisters = 2;
+const uint8_t numHoldingRegisters = 8;
 const uint8_t numInputRegisters = 8;
 bool coils[numCoils];
 bool discreteInputs[numDiscreteInputs];
@@ -49,20 +50,29 @@ uint16_t holdingRegisters[numHoldingRegisters];
 uint16_t inputRegisters[numInputRegisters];
 miniEEPROM eeprom(holdingRegisters, 2, 0); // 0-Device-ID , 1-BaudRate
 
+
+
 void ModbusInit(){
   modbus.configureCoils(coils, numCoils);
   modbus.configureDiscreteInputs(discreteInputs, numDiscreteInputs);
   modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);
   modbus.configureInputRegisters(inputRegisters, numInputRegisters);
-  MODBUS_SERIAL.begin(MODBUS_BAUD, MODBUS_CONFIG);
-  modbus.begin(MODBUS_UNIT_ID, MODBUS_BAUD, MODBUS_CONFIG);
+  eeprom.load();
+  holdingRegisters[1] = holdingRegisters[1] < 0 || holdingRegisters[1] > 7 ? 5 : holdingRegisters[1];
+  MODBUS_SERIAL.begin(USART_BAUD[holdingRegisters[1]], MODBUS_CONFIG);
+  modbus.begin(holdingRegisters[0],USART_BAUD[holdingRegisters[1]], MODBUS_CONFIG);
 }
   
 void ModbusUpdate(){
   gpio.WirteCoils(coils,numCoils);
   gpio.ReadDiscreteInputs(discreteInputs,numDiscreteInputs);
-  gpio.SetDisplay((int)holdingRegisters[0]);
+  gpio.SetDisplay((int)holdingRegisters[2]);
+  if(holdingRegisters[2] < 0 || holdingRegisters[2] > 9999) {
+    gpio.OffDisplay();
+  }
   gpio.ReadInputRegisters(inputRegisters,8);
+  eeprom.save();
+
 }
 
 void setup() {
